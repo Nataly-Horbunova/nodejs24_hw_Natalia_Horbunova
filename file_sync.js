83d2b9099bc5bfbb1ef096e45a19242d8463616d
Natalia Horbunova <nata.horbunova@gmail.com>
2024-02-01 23:26:30 +0200

    functions helpers added

--- file_sync.js
+++ file_sync.js
@@ -1,8 +1,27 @@
 
-    async function start () {
-        
-    }
+const fsAsync = require('fs/promises');
+const path = require('path');
 
+function doesExist(item, arr) {
+    return arr.includes(item);
+}
+
+async function isDir(path) {
+    const stat = await fsAsync.stat(path)
+    return stat.isDirectory();
+}
+
+async function syncFiles(sourceDir, tragetDir) {
+
+
+}
+
+async function start () {
+    const sourceDir = path.join('.', 'source');
+    const tragetDir = path.join('.', 'target');
+
+    // await syncFiles(sourceDir, tragetDir);
+}
 
 module.exports = {
     start
--- main.js
+++ main.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const logger = require('./utils/logger')('main');
 const fileSync = require('./file_sync');
 
-logger.info('the script is running!');
-logger.warn('warning message', { contextId: 42});
-logger.error(new Error('bad things happened', { cause: 'server broke a leg' }));
+// logger.info('the script is running!');
+// logger.warn('warning message', { contextId: 42});
+// logger.error(new Error('bad things happened', { cause: 'server broke a leg' }));
 
 fileSync.start();
 
